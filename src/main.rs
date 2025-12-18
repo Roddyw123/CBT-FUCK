70e1f7556038c798b4113424195524d1ba0f1e8a
William <wys24@ic.ac.uk>
2025-12-18 16:54:07 +0000

    [wys24] feat: change main to process and write to arbitrary files

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 mod bf2c;
 use bf2c::bf2c::bf2cify;
-use std::fs;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::env;
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
-    let contents = fs::read_to_string("src/bf.bf").expect("Unable to read file");
-    let result = bf2cify(contents).expect("failed to bf2cify");
-    let mut file = File::create("c.c").unwrap();
-    file.write_all(result.as_ref()).unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input.bf> <output.c>", args[0]);
+        process::exit(1);
+    }
+
+    let mut input = File::open(&args[1])
+        .expect("Unable to open input file");
+    
+    let mut contents = String::new();
+    input.read_to_string(&mut contents)
+        .expect("Failed to read input file");
+
+    let result = bf2cify(contents)
+        .expect("failed to bf2cify");
+
+    let mut output = File::create(&args[2])
+        .expect("could not create output file");
+
+    output.write_all(result.as_bytes()).unwrap();
 }
