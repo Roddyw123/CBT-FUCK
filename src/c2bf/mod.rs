dad87c731347fd443fd29b2beb50675e4ea28eeb
Andy <po.chung24@imperial.ac.uk>
2025-12-19 00:50:06 +0000

    feat: added parsing for function declaration and statements

--- src/c2bf/mod.rs
+++ src/c2bf/mod.rs
@@ -48,7 +48,7 @@ pub mod c2bf {
     }
 
     pub fn parser<'src, I: Input<'src>, E: ParserExtra<'src, I>>() -> impl Parser<'src, &'src str, Vec<GStmt<'src>>, Err<Cheap>> {
-       // not mapped to Var immediatly as it can be a function as well
+        // not mapped to Var immediatly as it can be a function as well
         let ident = text::ascii::ident()
             .padded();
 
@@ -59,9 +59,11 @@ pub mod c2bf {
 
         let open_bracket = just('(').padded();
         let close_bracket = just(')').padded();
+        let open_curly_bracket = just('{').padded();
+        let close_curly_bracket = just('}').padded();
         let open_square_bracket = just('[').padded();
         let close_square_bracket = just(']').padded();
-        
+
         let expr = recursive(|expr| {
 
             let atom = recursive(|atom| {
@@ -116,10 +118,96 @@ pub mod c2bf {
             )
             .then_ignore(sep);
 
+        fn block_help<'src, I: Input<'src>, E: ParserExtra<'src, I>, S, O, C>(stmt: S, open: O, close: C) -> impl Parser<'src, I, Vec<LStmt<'src>>, E> + Clone
+        where
+            S: Parser<'src, I, LStmt<'src>, E> + Clone,
+            O: Parser<'src, I, char, E> + Clone,
+            C: Parser<'src, I, char, E> + Clone,
+        {
+            stmt.clone()
+                .repeated()
+                .collect::<Vec<LStmt<'src>>>()
+                .delimited_by(open, close)
+        }
+
+        let func_dec_help = |stmt| types.clone()
+                .then(ident)
+                .then(
+                    typed_variable
+                    .separated_by(just(',').padded())
+                    .allow_trailing()
+                    .collect::<Vec<_>>()
+                    .delimited_by(open_bracket, close_bracket)
+                )
+                .then(block_help(stmt, open_curly_bracket, close_curly_bracket).clone());
+        
+        let local_stmt = recursive(|stmt| {
+            let block = block_help(stmt.clone(), open_curly_bracket, close_curly_bracket);
+
+            let x_statment = |name| keyword(name).padded()
+                .ignore_then(
+                    expr.clone()
+                    .delimited_by(open_bracket, close_bracket)
+                )
+                .then(block.clone());
+            let if_statment = x_statment("if").clone()
+                .then(
+                    keyword("else").padded()
+                    .ignore_then(x_statment("if").clone())
+                    .repeated().collect::<Vec<(Expr, Vec<LStmt>)>>()
+                )
+                .then(
+                    keyword("else").padded()
+                    .ignore_then(block.clone())
+                    .or_not()
+                );
+            let for_loop = keyword("for").padded()
+                .ignore_then(
+                    stmt.clone().or_not()
+                    .then_ignore(sep)
+                    .then(stmt.clone().or_not())
+                    .then_ignore(sep)
+                    .then(stmt.clone().or_not())
+                    .delimited_by(open_bracket, close_bracket)
+                )
+                .then(block.clone());
+            let func_dec = func_dec_help.clone()(stmt);
+            let assignment = ident.clone()
+                .then(
+                    just('=').padded()
+                    .ignore_then(expr.clone())
+                )
+                .then_ignore(sep);
+            choice((
+                declaration.clone()
+                    .map(|(((ty, name), arr), exp)|
+                        LStmt::Dec(ty, name, arr, exp)),
+                x_statment("while")
+                    .map(|(cond, body)|
+                        LStmt::While(cond, body)),
+                if_statment
+                    .map(|((e1, e2), else_tail)|
+                        LStmt::Ifs(e1, e2, else_tail)),
+                for_loop
+                    .map(|(((e1, e2), e3),body)|
+                        LStmt::For(e1.map(Box::new), e2.map(Box::new), e3.map(Box::new), body)),
+                func_dec.clone()
+                    .map(|(((ty,name ), params), body)|
+                        LStmt::FuncDec(ty, name, params.into_iter().map(|((ty, name), arr)| (ty, name, arr)).collect(), body)),
+                expr.clone()
+                    .map(LStmt::Expr),
+                assignment
+                    .map(|(name, exp)|
+                        LStmt::Assignment(name, exp)),
+            ))
+        });
         let global_stmts = choice((
             declaration.clone()
                 .map(|(((ty, name), arr), exp)|
                     GStmt::VarDec(ty, name, arr, exp)),
+            func_dec_help(local_stmt)
+                .map(|(((ty,name ), params), body)|
+                    GStmt::FuncDec(ty, name, params.into_iter().map(|((ty, name), arr)| (ty, name, arr)).collect(), body)),
         ))
         .repeated()
         .collect::<Vec<GStmt>>();
@@ -204,6 +292,104 @@ pub mod c2bf {
         fn global_char_variable_declaration_assignment_missing_semicolon_fail_test() {
             let stmts = parser::<&str, Err<EmptyErr>>().parse("char e = v").into_result();
             assert!(stmts.is_err());
+        }
+
+        // Function Declaration Tests
+        #[test]
+        fn global_function_declaration_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(){}").into_result();
+            assert_eq!(stmts, Ok(vec![GStmt::FuncDec(Type::Char, "foo", Vec::new(), Vec::new())]));
+        }
+
+        #[test]
+        fn global_function_declaration_missing_return_type_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("foo(){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_missing_name_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char (){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_missing_open_bracket_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_missing_closed_bracket_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo({}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_extra_open_bracket_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo((){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_extra_closed_bracket_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo()){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_missing_open_curly_bracket_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo()}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_missing_closed_curly_bracket_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(){").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_1_parameter_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(char a){}").into_result();
+            assert_eq!(stmts, Ok(vec![GStmt::FuncDec(Type::Char, "foo", vec![(Type::Char, "a", None)], Vec::new())]));
+        }
+
+        #[test]
+        fn global_function_declaration_1_parameter_trailing_comma_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(char a,){}").into_result();
+            assert_eq!(stmts, Ok(vec![GStmt::FuncDec(Type::Char, "foo", vec![(Type::Char, "a", None)], Vec::new())]));
+        }
+
+        // failing parameters
+        #[test]
+        fn global_function_declaration_parameter_missing_type_fail_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(a){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_parameter_missing_name_fail_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(char){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_single_comma_fail_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(,){}").into_result();
+            assert!(stmts.is_err());
+        }
+
+        #[test]
+        fn global_function_declaration_2_parameters_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(char a, char b){}").into_result();
+            assert_eq!(stmts, Ok(vec![GStmt::FuncDec(Type::Char, "foo", vec![(Type::Char, "a", None), (Type::Char, "b", None)], Vec::new())]));
+        }
+
+        #[test]
+        fn global_function_declaration_2_parameters_trailing_comma_test() {
+            let stmts = parser::<&str, Err<EmptyErr>>().parse("char foo(char a, char b, ){}").into_result();
+            assert_eq!(stmts, Ok(vec![GStmt::FuncDec(Type::Char, "foo", vec![(Type::Char, "a", None), (Type::Char, "b", None)], Vec::new())]));
         }
     }
 }
