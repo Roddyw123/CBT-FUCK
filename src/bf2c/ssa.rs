c1f41096579e7100e7cbe74f9f72d53f24726c20
Roddyw123 <yuitiwata@gmail.com>
2026-01-05 15:44:59 +0000

    [rw2024] chore: added comments to ssabuilder functions for clarity

--- src/bf2c/ssa.rs
+++ src/bf2c/ssa.rs
@@ -43,33 +43,37 @@ pub struct PhiNode {
 
 /// SSA Builder - transforms IR to SSA form
 pub struct SsaBuilder {
-    /// Current version for each cell offset
-    versions: HashMap<i32, u32>,
-    /// Current pointer offset
-    ptr_offset: i32,
+    versions: HashMap<i32, u32>, // Maps cell->offset
+    ptr_offset: i32, // Tracks current poiner offset
 }
 
 
 // SSA Builder Methods: Wrap important IR to SSA IR conversions
 impl SsaBuilder {
+    // Wrapper around convert_ssa
     fn ssa(&mut self, r: Prog) -> SsaProg {
         self.convert_ssa(r)
     }
 
+    // Returns the current SSA Variable (offset, version) at the cell at cell_offset. Returns 0
+    // if there is no ssa variable at the cell_offset return version=0
     fn get_offset_var(&self, cell_offset: i32) -> SsaVar {
         (cell_offset, self.versions.get(&cell_offset).copied().unwrap_or(0))
     }
 
+    // Returns the current pointer offset
     fn current_cell(&self) -> i32 {
         self.ptr_offset
     }
-    
+
+    // Increments version counter for a cell and returns new SSA variable
     fn new_var(&mut self, cell_offset: i32) -> SsaVar{
         let n = self.versions.get(&cell_offset).copied().unwrap_or(0) + 1;
         self.versions.insert(cell_offset, n);
         (cell_offset, n)
     }
 
+    // Returns an SsaProg by processing a program (vector of IR) in Haskell, case by case.
     fn convert_ssa(&mut self, r: Prog) -> SsaProg {
         let mut output: SsaProg = Vec::new();
         for ir in r { 
@@ -93,7 +97,7 @@ impl SsaBuilder {
                     output.push(SsaStmt::Input(new));
                 }
                 Stmt::Loop(body) => {
-                    todo!()
+                    todo!();
                 }
                 Stmt::ZeroLoop => { 
                     let new = self.new_var(self.ptr_offset);
